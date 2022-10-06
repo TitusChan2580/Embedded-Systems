76a683cab9c90e5998e47c81c138b600194be3f3
TitusChan2580 <tituschantaichun@gmail.com>
2022-10-06 12:01:23 +0100

    Task 101-1
    made the blinky lights  flash red+green then blue repeatedly

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -5,8 +5,8 @@ DigitalOut greenLED(LED1);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -18,14 +18,28 @@ int main()
 
         // Turn onboard LED ON  
         greenLED = 1; 
+        redLED = 1;
 
         // Wait 0.5 second (1 million microseconds)
         wait_us(200000); 
 
         // Turn LED OFF
         greenLED = 0;
+        redLED = 0;
 
         // Wait 0.5 second
         wait_us(200000); 
+
+        // Turn on Blue Led
+        blueLED = 1;
+
+        //wait 0.5 second
+        wait_us(200000);
+
+        //turn blue led off
+        blueLED = 0;
+
+        //wait 0.5 second
+        wait_us(200000);
     }
 }
