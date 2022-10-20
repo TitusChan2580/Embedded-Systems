309882a259f7a3546e7c36a8f14eb96b375dc92f
TitusChan2580 <tituschantaichun@gmail.com>
2022-10-20 12:50:12 +0100

    task 116
    completed task 116

--- Tasks/Task-116-PortOut/main.cpp
+++ Tasks/Task-116-PortOut/main.cpp
@@ -5,15 +5,15 @@ PortOut lights(PortC, 0b0000000001001100);
 int main()
 {
     //All OFF
-    lights = 0;
+    lights = 4;
 
     while (true)
     {
-        lights = 0b0000000000000100;
+        lights = 0b0000000000000100 + 0b0000000000001000;
         wait_us(1000000);
-        lights = 0b0000000000001000;
+        lights = 0b0000000000001000 + 0b0000000001000000;
         wait_us(1000000);
-        lights = 0b0000000001000000;
+        lights = 0b0000000001000000 + 0b0000000000000100;
         wait_us(1000000);                
     }
 }
