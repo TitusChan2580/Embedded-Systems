132b13d0041cb8f49ffab2e4c352a33487c98782
TitusChan2580 <tituschantaichun@gmail.com>
2022-10-20 12:23:33 +0100

    Task 115
    completed task 115

--- Tasks/Task-115-BusOut/main.cpp
+++ Tasks/Task-115-BusOut/main.cpp
@@ -2,12 +2,17 @@
 #define WAIT_TIME_MS 1000 
 
 
-// DigitalOut redLED(PC_2,1);
-// DigitalOut yellowLED(PC_3,1);
-// DigitalOut greenLED(PC_6,1);
+ //DigitalOut redLED(PC_2,1);
+ //DigitalOut yellowLED(PC_3,1);
+ //DigitalOut greenLED(PC_6,1);
 
-BusOut lights(PC_2, PC_3, PC_6);
-//BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
+
+// for 0 1 2 3 4 5 6 7 they all increace in binary units
+
+
+
+//BusOut lights(PC_2, PC_3, PC_6);
+BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 
 int main()
 {
@@ -16,14 +21,14 @@ int main()
 
     while (true)
     {
-        int count = 0;
-        while (count <= 7) 
+        int count = 31;
+        while (count >= 0) 
         {
             printf("count = %d\n", count);
             lights = count;
-            wait_us(1000000);
+            wait_us(250000);
 
-            count = count + 1;
+            count = count - 1;
         }
     }
 }
