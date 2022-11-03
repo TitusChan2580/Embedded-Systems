20726f9419ff7c66c98f9e8a8b7cedcf2095817f
TitusChan2580 <tituschantaichun@gmail.com>
2022-11-03 12:48:35 +0000

    task 156
    completed

--- Tasks/Task-156-Arrays/main.cpp
+++ Tasks/Task-156-Arrays/main.cpp
@@ -12,7 +12,7 @@ DigitalOut greenLED(TRAF_GRN1_PIN);
 //Analogue Inputs
 AnalogIn ldr(AN_LDR_PIN);
 Buzzer buzz;
-
+double value =0 ;
 
 int main()
 {
@@ -21,6 +21,7 @@ int main()
     for (unsigned int m=0; m<100; m++) {
         printf("%X ", samples[m]);
     }
+    
 
     // Automatic headlamp 
     while (true) {
@@ -28,14 +29,23 @@ int main()
         for (unsigned int m=0; m<100; m++) {
             unsigned short ldrVal   = ldr.read_u16();
             samples[m] = ldrVal;
+            value = value + ldrVal ;
             wait_us(10000);          // 10ms
         }
 
         // TASK a. Calculate the average value in samples
-
+        int avrVal = value / 100 ;
+        printf ("\nthe average is %X \n", avrVal ) ;
+        printf ("in decimal it is %d \n", avrVal );
         // TASK b. Display to 1dp
-
+        double avrVald = value / 100 ;
+        printf ("with decimals it is %f\n", avrVald ) ;
         // TASK c. Switch green LED on when dark;
+    if(avrVal > 35000 ){
+        greenLED=1;
+    }
+    else{greenLED=0;}
+    break;
 
     }  
 }
